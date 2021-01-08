4c48ddb2f48494891b8e1c50434a722c8a5cde9c
ZaqueuPS <zaqueu@gestaoopme.com.br>
2021-01-08 20:38:09 -0300

    finalizando pagina de login

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { TextField, Button } from "@material-ui/core";
 import { ContainerCustom, CardCustom } from "./styles";
+import EmailIcon from '@material-ui/icons/EmailOutlined';
+import PassIcon from '@material-ui/icons/LockOutlined';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 export default function LoginPage() {
   return (
     <ContainerCustom>
       <CardCustom>
         <h1>Login RH</h1>
+        <TextField label="E-mail" variant="outlined" InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <EmailIcon />
+            </InputAdornment>
+          ),
+        }}  />
+        <br/>
+        <TextField label="Senha" variant="outlined" type="password"
+      InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <PassIcon />
+            </InputAdornment>
+          ),
+        }} 
+         />
+        <br/>
+        <Button variant="contained" color="primary" size="medium">
+            Entrar
+        </Button>
       </CardCustom>
     </ContainerCustom>
   );
--- src/pages/LoginPage/styles.js
+++ src/pages/LoginPage/styles.js
@@ -1,13 +1,27 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { Container, Card, TextField, Button } from "@material-ui/core";
 
 export const ContainerCustom = styled(Container)`
-    padding:15px;
+  padding: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* height: calc(100vh - 120px); */
+  height: 100vh;
 `;
 export const CardCustom = styled(Card)`
-    with:50%;
-    max-width:230px;
-    height:50%;
-    max-height: 300px;
+  width: 50%;
+  min-width:320px;
+  max-width: 400px;
+  height: 50%;
+  max-height: 500px;
+  min-height:320px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction:column;
 
-`
+  .MuiTextField-root{
+      width:80%;
+  }
+`;
